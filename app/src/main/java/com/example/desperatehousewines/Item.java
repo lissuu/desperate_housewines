515a619401754775dc861c07193a960f36c22336
Niko J <lokkiju@gmail.com>
2020-11-24 09:47:11 +0200

    Implement JSON parser and getters for Item class
    - Moved Type enum inside Item class
    - Type enum has now a field for converting it to string
    - Primary parser hashmap now accepts JSON keys and excel column indices are now mapped to them
    - "Nimi" key is no longer converted to lowercase
    - Data stored from "Nimi" to name field is now stripped from numbers
    - get*AsString returns values with units and empty strings if no values are stored
    - Fixed commenting here and there

--- app/src/main/java/com/example/desperatehousewines/Item.java
+++ app/src/main/java/com/example/desperatehousewines/Item.java
@@ -13,32 +13,40 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.function.Consumer;
 
-enum Type {
-    NONE,
-    RED_WINE,
-    ROSEE_WINE,
-    WHITE_WINE,
-    RUM,
-    COGNAC,
-    WHISKEY,
-    BEER,
-    CIDER,
-    BLEND,
-    NON_ALCOHOL,
-    OTHER,
-    DESSERT_WINE,
-    BRANDY,
-    GIN,
-    LIQUOR,
-    SPARKLING_WINE,
-    VODKA
-}
+
 
 public class Item {
     final String TAG = "ITEM";
 
+    public enum Type {
+        NONE(""),
+        RED_WINE("Punaviini"),
+        ROSEE_WINE("Roseeviini"),
+        WHITE_WINE("Valkoviini"),
+        RUM("Rommi"),
+        COGNAC("Konjakki"),
+        WHISKEY("Viski"),
+        BEER("Olut"),
+        CIDER("Siideri"),
+        BLEND("Sekoitus"),
+        NON_ALCOHOL("Alkoholiton"),
+        OTHER("Muu"),
+        DESSERT_WINE("Jälkiruokaviini"),
+        BRANDY("Brandy"),
+        GIN("Gini"),
+        LIQUOR("Likööri"),
+        SPARKLING_WINE("Kuohuviini"),
+        VODKA("Vodka");
+
+        private String name;
+
+        Type(String n) {
+            this.name = n;
+        }
+    }
     /*
-            Column                  Variable    Data Type   Raw Data Example                                Notes
+
+        Idx XML Header/JSON Key     Variable    Data Type   Raw Data Example                                Notes
             ------------------------------------------------------------------------------------------------------------------
         0   Numero	                number      long        945096                                          Is unique
         1   Nimi	                name        String      Adrianna Vineyard Fortuna Terrae Malbec 2015    -
@@ -75,50 +83,18 @@ public class Item {
         27  Energia kcal/100 ml     -                       80.0                                            -
         28  Valikoima               selection   String      tilausvalikoima                                 -
         29  EAN                     ean         long        7794450005274                                   Almost unique, but less than 'Numero'.
-     */
 
-    /*  Parser HashMap
-            Key     Cell's index, see the table above.
+
+    Parser HashMap
+            Key     JSON's key
             Value   Standard functional interface Consumer<T> and implemented as Consumer<String> because
                     Cell.getRawValue() returns always a String which is then parsed by the implementation.
 
                     Consumer<T> details: https://docs.oracle.com/javase/8/docs/api/java/util/function/Consumer.html
      */
-    Map<Integer, Consumer<String>> parsers = new HashMap<Integer, Consumer<String>>() {{
-        put(0, (String r) -> number = parseValue(r, 1L, true));
-        put(1, (String r) -> name = parseValue(r, "", true));
-        put(2, (String r) -> producer = parseValue(r, "", false));
-        put(3, (String r) -> size = parseValue(r.split(" ", -1)[0].replace(",", "."), 0.0f, false));
-        put(4, (String r) -> price = parseValue(r, 0.0f, true));
-
-        put(6, (String r) -> isNew = parseValue(r, false, false, "uutuus"));
-        put(8, (String r) -> type = parseValue(r, Type.NONE, true));
-        put(9, (String r) -> subType = parseValue(r, "", false));
-
-        put(12, (String r) -> country = parseValue(r, "", false));
-        put(13, (String r) -> area = parseValue(r, "", false));
-        put(14, (String r) -> year = parseValue(r, 0, false));
-
-        put(15, (String r) -> extraLabel = parseValue(r, "", false));
-        put(16, (String r) -> extraInfo = parseValue(r, "", false));
-        put(17, (String r) -> grapes = parseValue(r.split(","), new ArrayList<String>(), false));
-        put(18, (String r) -> keywords = parseValue(r.split(","), new ArrayList<String>(), false));
-        put(19, (String r) -> packageType = parseValue(r, "", false));
-
-        put(20, (String r) -> stopper = parseValue(r, "", false));
-        put(21, (String r) -> alcohol = parseValue(r, -1, false));
-
-        put(28, (String r) -> selection = parseValue(r, "", false));
-        put(29, (String r) -> ean = parseValue(r, 0L, false));
-    }};
-
-    /*  Parsers for JSON objects
-            Key     JSON key, case sensitive.
-            Value   see above.
-     */
     Map<String, Consumer<String>> jsonParsers = new HashMap<String, Consumer<String>>() {{
         put("Numero", (String r) -> number = parseValue(r, 1L, true));
-        put("Nimi", (String r) -> name = parseValue(r, "", true));
+        put("Nimi", (String r) -> name = parseValue(r, "", true).replaceAll("[0123456789]", ""));
         put("Valmistaja", (String r) -> producer = parseValue(r, "", false));
         put("Pullokoko", (String r) -> size = parseValue(r.split(" ", -1)[0].replace(",", "."), 0.0f, false));
         put("Hinta", (String r) -> price = parseValue(r, 0.0f, true));
@@ -142,6 +118,35 @@ public class Item {
 
         put("Valikoima", (String r) -> selection = parseValue(r, "", false));
         put("EAN", (String r) -> ean = parseValue(r, 0L, false));
+    }};
+
+    // Map excel columns to JSON keys.
+    Map<Integer, String> indexToKey = new HashMap<Integer, String>() {{
+        put(0, "Numero");
+        put(1, "Nimi");
+        put(2, "Valmistaja");
+        put(3, "Pullokoko");
+        put(4, "Hinta");
+
+        put(6, "Uutuus");
+        put(8, "Tyyppi");
+        put(9, "Alatyyppi");
+
+        put(12, "Valmistusmaa");
+        put(13, "Alue");
+        put(14, "Vuosikerta");
+
+        put(15, "Etikettimerkintoja");
+        put(16, "Huomautus");
+        put(17, "Rypaleet");
+        put(18, "Luonnehdinta");
+        put(19, "Pakkaustyyppi");
+
+        put(20, "Suljentatyyppi");
+        put(21, "Alkoholi_prosentti");
+
+        put(28, "Valikoima");
+        put(29, "EAN");
     }};
 
     // Starts as true (valid) and set to false by the parsers if the data is required.
@@ -176,17 +181,18 @@ public class Item {
     // Constructor for parsing excel sheet
     public Item (Row r) {
         // Iterates through parsers and tries to fetch data from the given row.
-        for (HashMap.Entry<Integer, Consumer<String>> e : parsers.entrySet()) {
-            Integer key = e.getKey();
-            Consumer<String> parser = e.getValue();
+        for (HashMap.Entry<Integer, String> e : indexToKey.entrySet()) {
+            int mapIndex = e.getKey();
+            String mapValue = e.getValue();
 
             // Parser is called with an empty string if no data is found for that index.
-            if (r.hasCell(key)) {
-                String raw = r.getCell(key).getRawValue();
-
-                parser.accept(raw != null ? raw : "");
-            } else {
-                parser.accept("");
+            if (jsonParsers.containsKey(mapValue)) {
+                if (r.hasCell(mapIndex)) {
+                    String raw = r.getCell(mapIndex).getRawValue();
+                    jsonParsers.get(mapValue).accept(raw != null ? raw : "");
+                } else {
+                    jsonParsers.get(mapValue).accept("");
+                }
             }
         }
     }
@@ -229,6 +235,35 @@ public class Item {
 
     public String getName () {
         return name;
+    }
+
+    public String getAlcoholAsString() {
+        return alcohol > 0 ? alcohol + " %" : "";
+    }
+
+    public String getSizeAsString() {
+        return size > 0 ? size + " l" : "";
+    }
+
+    public String getPriceAsString() {
+        return price >  0 ? price + " e" : "";
+    }
+
+    public boolean isWine() {
+        switch (type) {
+            case RED_WINE:
+            case ROSEE_WINE:
+            case WHITE_WINE:
+            case DESSERT_WINE:
+            case SPARKLING_WINE:
+                return true;
+        }
+
+        return false;
+    }
+
+    public String getYearAsString() {
+        return year > 0 ? String.valueOf(year) : "";
     }
 
     private int parseValue (String str, int def, boolean isRequired) {
@@ -290,7 +325,7 @@ public class Item {
             return def;
         }
 
-        String trimmed = str.trim().toLowerCase();
+        String trimmed = str.trim();
 
         if (trimmed.isEmpty() || trimmed == "null") {
             if (isRequired)
