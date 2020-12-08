5dbc71bcd9806108807b4d537268f6c3cc250be7
Niko J <lokkiju@gmail.com>
2020-12-08 13:25:17 +0200

    Implement AsyncTask for JSON parsing and move RestClient to hold all data and relevant methods for it
    It's still a mess, but a better mess.
    
    - A dialog now pops and closes when JSON is downloaded, it has a cool spinner and everything!
    - Another dialog, with progress bar, is shown when JSON is being parsed which can take some time, depending on your hardware
    - Data is now held in RestClient, pending a better solution
    - Parsing is now done in an asynctask to show the user something is happening and nothing crashed
    - Removed the parameter from RestClient's callback, it no longer has a purpose in there
    - API enum has dialog messages in it, but these are subject to change

--- app/src/main/java/com/example/desperatehousewines/BrowseActivity.java
+++ app/src/main/java/com/example/desperatehousewines/BrowseActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +16,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.VolleyError;
+import com.ctc.wstx.io.AsciiReader;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,10 +25,12 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.view.KeyEvent.KEYCODE_ENTER;
+
 public class BrowseActivity extends AppCompatActivity implements RestClient.Callback {
     static final String TAG = "BROWSE";
 
-    List<Item> items = new ArrayList<Item>();
+    protected List<Item> items = new ArrayList<Item>();
 
     Context context;
     RecyclerView recyclerView;
@@ -44,98 +49,61 @@ public class BrowseActivity extends AppCompatActivity implements RestClient.Call
         btnSearch = findViewById(R.id.btnSearch);
         btnClearSearch = findViewById(R.id.btnClearSearch);
 
-        // Fetches data from the API and calls RestClient.Callback implementation when Volley is done.
-        RestClient.getInstance(this).get(RestClient.API.LIST, this);
         recyclerView.setLayoutManager(new GridLayoutManager(context, 2));
 
+        // Fetches data from the API and calls RestClient.Callback implementation when Volley is done.
+        RestClient.getInstance(this).get(RestClient.API.LIST, this);
+
         /*
-         * ON CLICK LISTENERS
+         * LISTENERS
          */
 
-        // Tries to determine from user given input what was meant to be searched. Search string is
-        // always lower cased and trimmed out of leading and trailing spaces.
         btnSearch.setOnClickListener(v -> {
-            closeKeyboard();
-            String search = inpSearch.getText().toString().toLowerCase().trim();
-            List<Item> result = new ArrayList<>();
-
-            if (search.equals("")) {
-                // Clear
-                updateRecyclerView(null);
-                return;
-            } else if (tryParseInt(search)) {
-                // Search by year
-                result = getItemsByYear(Integer.parseInt(search));
-            } else {
-                // Search by name
-                result = getItemsByName(search);
-            }
-
-            Toast.makeText(this, result.size() > 0 ? "Osumia " + result.size() + " kappaletta" : "Haulla ei löytynyt yhtään tuotetta", Toast.LENGTH_LONG).show();
-            updateRecyclerView(result);
+            doSearch();
         });
 
-        // Function for clearing search input and restoring view to its non-searched look.
+        inpSearch.setOnKeyListener((v, keyCode, event) -> {
+            if (keyCode == KEYCODE_ENTER)
+                doSearch();
+
+            return false; // passed lambda for setOnKeyListener requires something to be returned, this value has no meaning in this context.
+        });
+
         btnClearSearch.setOnClickListener(v -> {
-            closeKeyboard();
-            inpSearch.setText("");
-            updateRecyclerView(null);
+            clearSearch();
         });
+    }
+
+    // Tries to determine from user given input what was meant to be searched. Search string is
+    // always lower cased and trimmed out of leading and trailing spaces.
+    private void doSearch() {
+        closeKeyboard();
+        String search = inpSearch.getText().toString().toLowerCase().trim();
+
+        if (search.equals("")) {
+            // Clear
+            updateRecyclerView(RestClient.getInstance(this).getItems());
+            return;
+        } else if (tryParseInt(search)) {
+            // Search by year
+            updateRecyclerView(RestClient.getInstance(this).getItemsByYear(Integer.parseInt(search)));
+        } else {
+            // Search by name
+            updateRecyclerView(RestClient.getInstance(this).getItemsByName(search));
+        }
+    }
+
+    // Function for clearing search input and restoring view to its non-searched look.
+    private void clearSearch() {
+        closeKeyboard();
+        inpSearch.setText("");
+        updateRecyclerView(null);
     }
 
     // Creates and sets a new adapter for recycler view. See RecyclerBrowseAdapter class for
     // further details.
-    private void updateRecyclerView (List<Item> tempList) {
-        if (tempList == null) {
-            Log.d(TAG, "updating recycler view with full list");
-            recyclerView.setAdapter(new RecyclerBrowseAdapter(context, items));
-        } else {
-            Log.d(TAG, "updating recycler view with temp/partial list");
-            recyclerView.setAdapter(new RecyclerBrowseAdapter(context, tempList));
-        }
-    }
-
-    // Searches items list by name with 'contains' method. Case is ignored and a match can be at any
-    // string position.
-    private List<Item> getItemsByName (String search) {
-        List<Item> result = new ArrayList<Item>();
-
-        if (search.equals(""))
-            return result;
-
-        Log.d(TAG, "name search called with '" + search + "'");
-
-        for (Item i : items) {
-            String itemName = i.getName().toLowerCase();
-
-            if (itemName.contains(search)) {
-                Log.d(TAG, "\t\t[" + result.size() + "]\t" + itemName);
-                result.add(i);
-            }
-        }
-
-        Log.d(TAG, "search returning with " + result.size() + " items");
-        return result;
-    }
-
-    // Searches items list by a year.
-    // Note: alko's api has a lot of blanks for this information
-    private List<Item> getItemsByYear (int search) {
-        List<Item> result = new ArrayList<Item>();
-
-        Log.d(TAG, "year search called with '" + search + "'");
-
-        for (Item i : items) {
-            int item = i.getYear();
-
-            if (item == search) {
-                Log.d(TAG, "\t\t[" + result.size() + "]\t" + item);
-                result.add(i);
-            }
-        }
-
-        Log.d(TAG, "search returning with " + result.size() + " items");
-        return result;
+    private void updateRecyclerView (List<Item> items) {
+        recyclerView.setAdapter(new RecyclerBrowseAdapter(context, items));
     }
 
     // Closes soft keyboard via InputMethodManager.
@@ -158,35 +126,10 @@ public class BrowseActivity extends AppCompatActivity implements RestClient.Call
         }
     }
 
-    // Debugging purposes only
-    private void itemsToString (List<Item> itemList) {
-        Log.d(TAG, "item list size: " + itemList.size());
-
-        for (Item i : itemList)
-            Log.d(TAG, i.toString());
-    }
-
     // RestClient.Callback implementation
     @Override
-    public void onResponse(JSONArray array) {
-        Log.d(TAG, "clearing " + items.size() + " items");
-        items.clear();
-
-        for (int i = 0; i < array.length(); i++) {
-            try {
-                JSONObject row = array.getJSONObject(i);
-                Item item = new Item(row);
-
-                if (item.isValid())
-                    items.add(item.createCardTitle());
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Log.d(TAG, "has " + items.size() + " items");
-        updateRecyclerView(null);
+    public void onResponse() {
+        updateRecyclerView(RestClient.getInstance(this).getItems());
     }
 
     // RestClient.Callback implementation
@@ -194,4 +137,5 @@ public class BrowseActivity extends AppCompatActivity implements RestClient.Call
     public void onError(VolleyError err) {
         Log.e(TAG, "volley error!");
     }
+
 }
--- app/src/main/java/com/example/desperatehousewines/RestClient.java
+++ app/src/main/java/com/example/desperatehousewines/RestClient.java
@@ -1,6 +1,8 @@
 package com.example.desperatehousewines;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -11,36 +13,44 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestClient {
     public enum API {
-        LIST("alko-lista", Request.Method.GET),
-        SEARCHES("haut", Request.Method.GET),
-        CONSUMERS("kuluttajat", Request.Method.GET),
-        DRINKS("omat-juomat", Request.Method.GET),
-        SERVICES("palvelut", Request.Method.GET);
+        LIST("alko-lista", Request.Method.GET, "Ladataan tietoja..."),
+        SEARCHES("haut", Request.Method.GET, ""),
+        CONSUMERS("kuluttajat", Request.Method.GET, ""),
+        DRINKS("omat-juomat", Request.Method.GET, ""),
+        SERVICES("palvelut", Request.Method.GET, "");
 
         private final String request;
         private final int method;
+        private final String dialogMessage;
 
         public static String server = "http://91.145.114.251:8001/";
 
-        API(String r, int m) {
+        API(String r, int m, String d) {
             this.request = r;
             this.method = m;
+            this.dialogMessage = d;
         }
 
         String url() {
             return server + this.request;
         }
-
         int method() {
             return method;
         }
+        String msg() { return dialogMessage; }
+        boolean hasMsg() { return dialogMessage.length() > 0; }
     }
 
     public interface Callback {
-        void onResponse(JSONArray array);
+        void onResponse();
         void onError(VolleyError err);
     }
 
@@ -48,11 +58,14 @@ public class RestClient {
 
     private static RestClient instance;
     private RequestQueue requestQueue;
-    private static Context ctx;
+    private static Context context;
     private JsonArrayRequest jsonArrayRequest;
+    private ProgressDialog progressBar;
+
+    private List<Item> items;
 
     private RestClient(Context context) {
-        ctx = context;
+        this.context = context;
         requestQueue = getRequestQueue();
     }
 
@@ -66,7 +79,7 @@ public class RestClient {
 
     public RequestQueue getRequestQueue() {
         if (requestQueue == null) {
-            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
         }
 
         return requestQueue;
@@ -75,19 +88,43 @@ public class RestClient {
     // Call rest API with a callback interface, use the overloaded below if you don't want to use an interface.
     public void get(API api, Callback cb) {
         Log.d(TAG, "get: " + api.toString() + ", url: " + api.url() + ", method: " + api.method());
+        progressBar = new ProgressDialog(context);
+
+        if (api.hasMsg()) {
+            progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressBar.setProgressNumberFormat(null);
+            progressBar.setMessage(api.msg());
+            progressBar.show();
+        }
 
         jsonArrayRequest = new JsonArrayRequest(api.method(), api.url(),
             new Response.Listener<JSONArray>() {
                 @Override
                 public void onResponse(JSONArray response) {
-                    Log.d(TAG, "volley done, calling cb");
-                    cb.onResponse(response);
+                    Log.d(TAG, "volley done");
+                    progressBar.dismiss();
+                    //closeDialog(api);
+
+                    switch (api) {
+                        case LIST:
+                            new AsyncTaskRunner(context, response, cb).execute();
+                            break;
+                        case DRINKS:
+                        case SEARCHES:
+                        case SERVICES:
+                        case CONSUMERS:
+                            cb.onResponse();
+                            break;
+                        default:
+                            Log.e(TAG, "api switch defaults");
+                    }
                 }
             },
             new Response.ErrorListener(){
                 @Override
                 public void onErrorResponse(VolleyError error){
                     Log.e(TAG, "volley error");
+                    progressBar.dismiss();
                     cb.onError(error);
                 }
             }
@@ -108,5 +145,147 @@ public class RestClient {
         );
 
         getRequestQueue().add(jsonArrayRequest);
+    }
+
+    public List<Item> getItems () {
+        return items;
+    }
+
+    // Searches items list by name with 'contains' method. Case is ignored and a match can be at any
+    // string position.
+    public List<Item> getItemsByName (String search) {
+        List<Item> result = new ArrayList<Item>();
+
+        if (search.equals(""))
+            return result;
+
+        Log.d(TAG, "name search called with '" + search + "'");
+
+        for (Item i : items) {
+            String itemName = i.getName().toLowerCase();
+
+            if (itemName.contains(search)) {
+                Log.d(TAG, "\t\t[" + result.size() + "]\t" + itemName);
+                result.add(i);
+            }
+        }
+
+        Log.d(TAG, "search returning with " + result.size() + " items");
+        return result;
+    }
+
+    // Searches items list by a year.
+    // Note: alko's api has a lot of blanks for this information
+    public List<Item> getItemsByYear (int search) {
+        List<Item> result = new ArrayList<Item>();
+
+        Log.d(TAG, "year search called with '" + search + "'");
+
+        for (Item i : items) {
+            int item = i.getYear();
+
+            if (item == search) {
+                Log.d(TAG, "\t\t[" + result.size() + "]\t" + item);
+                result.add(i);
+            }
+        }
+
+        Log.d(TAG, "search returning with " + result.size() + " items");
+        return result;
+    }
+
+    // Debugging purposes only
+    public void itemsToString (List<Item> itemList) {
+        Log.d(TAG, "item list size: " + itemList.size());
+
+        for (Item i : itemList)
+            Log.d(TAG, i.toString());
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, Integer, String> {
+        private Context context;
+        private JSONArray array;
+        private ProgressDialog progressBar;
+        private Callback cb;
+
+        public AsyncTaskRunner (Context context, JSONArray array, Callback cb) {
+            this.context = context;
+            this.array = array;
+            this.cb = cb;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.d(TAG, "onPreExecute");
+
+            // Initialize progress bar.
+            progressBar = new ProgressDialog(context);
+            progressBar.setMessage("Käsitellään tietoja...");
+            progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressBar.setProgressNumberFormat(null);
+            progressBar.setProgress(0);
+            progressBar.setMax(100);
+            progressBar.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            items = new ArrayList<Item>();
+
+            int len = array.length();
+            int prevProgress = -1;
+            int nowProgress = -1;
+            int notValidItems = 0;
+
+            // Loop through data and update progress.
+            for (int i = 0; i < len; i++) {
+                try {
+                    JSONObject row = array.getJSONObject(i);
+                    Item item = new Item(row);
+
+                    if (item.isValid()) {
+                        items.add(item.createCardTitle());
+                    } else {
+                        notValidItems++;
+                    }
+
+                    nowProgress = (int) Math.round((double) i / len * 100);
+
+                    // No need to update progress 10000 times when there is only 100 different progress values.
+                    if (prevProgress != nowProgress) {
+                        prevProgress = nowProgress;
+                        publishProgress(nowProgress);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            // Sleeping a thread can cause an exception and thus needs to be enclosed in try catch.
+            try {
+                // The user needs to see 100%. For some reason.
+                Thread.sleep(500);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Log.d(TAG, "parsed " + items.size() + " valid items and dismissed " + notValidItems + " items");
+            progressBar.dismiss();
+            return "done";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(TAG, "onPostExecute: " + result);
+            cb.onResponse();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            Log.d(TAG, "progress: " + progress[0] + "%");
+            progressBar.setProgress(progress[0]);
+        }
     }
 }
